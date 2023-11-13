cb20fb6686a3b36ef0f37dd6d8bd57dfb82d53d0
Faisal25DEC <faisalmd25121999@gmail.com>
2023-11-13 16:46:13 +0530

    backend updates

--- index.js
+++ index.js
@@ -18,12 +18,12 @@ require("dotenv").config();
 const PORT = process.env.PORT;
 
 const app = express();
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "https://instaclonevi.netlify.app/");
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.header("Access-Control-Allow-Headers", "Content-Type");
+//   next();
+// });
 
 app.use(
   cors({
